7b45be7a743d96c77e5fcc4c8af5b3a37783bbbf
Prateek Tandon <erprateek@github.com>
2025-04-20 08:39:01 -0700

    Adding redis backend to celery worker

--- master/celery_worker.py
+++ master/celery_worker.py
@@ -3,8 +3,9 @@ import os
 
 broker = os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0")
 
-celery = Celery("tasks", broker=broker)
+celery = Celery("tasks", broker=broker, backend=broker)
 
 @celery.task
 def add(x, y):
     return x + y
+
