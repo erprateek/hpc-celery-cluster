c34a6b31700c6526ab3362ff46766ae6c6abe67a
Prateek Tandon <erprateek@github.com>
2025-04-20 16:06:11 -0700

    Remove unwanted space

--- master/celery_worker.py
+++ master/celery_worker.py
@@ -5,7 +5,6 @@ broker = os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0")
 
 celery_app: Celery = Celery("tasks", broker=broker, backend=broker)
 
-
 @celery_app.task(name="celery_worker.add") # type: ignore
 def add(x: int, y: int) -> int:
     return x + y
