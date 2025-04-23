a72a21b1af9fff452fc42bb6d11da225e4a3dbc4
Prateek Tandon <ptandon1@andrew.cmu.edu>
2025-04-23 08:42:39 -0700

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,2 +1,9 @@
-CELERY_TASK_ALWAYS_EAGER = True  # newer versions
-CELERY_TASK_EAGER_PROPAGATES = True
+import pytest
+
+@pytest.fixture(autouse=True, scope="session")
+def configure_celery_for_testing():
+    from hpc_celery_cluster.main import celery_app
+    celery_app.conf.update(
+        task_always_eager=True,
+        task_eager_propagates=True,
+    )
