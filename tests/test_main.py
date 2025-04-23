f89220f108e8ba6aa0655bbc2023ae01d7a87635
Prateek Tandon <ptandon1@andrew.cmu.edu>
2025-04-23 08:37:14 -0700

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,12 @@
-from fastapi.testclient import TestClient
 import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../master")))
-from main import app
 import time
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../master"))) 
+from main import app  # noqa: E402
+from fastapi.testclient import TestClient
 
 client = TestClient(app)
+
 
 def test_add_task():
     print("Reached here: ")
