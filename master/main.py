d69ef4aedc1a1705f431241fcf0b5868a234df08
Prateek Tandon <ptandon1@andrew.cmu.edu>
2025-04-23 08:30:35 -0700

    Update main.py

--- master/main.py
+++ master/main.py
@@ -1,18 +1,21 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from pydantic import BaseModel
 from celery.result import AsyncResult
 from celery_worker import celery_app
 from typing import Any, Dict
 app = FastAPI()
 
+
 class AddRequest(BaseModel):
     x: int
     y: int
+
 
 @app.post("/add")
 def add(request: AddRequest) -> Dict[str, str]:
     task = celery_app.send_task("celery_worker.add", args=[request.x, request.y])
     return {"task_id": task.id}
+
 
 @app.get("/result/{task_id}")
 def get_result(task_id: str) -> Dict[str, Any | None]:
@@ -22,7 +25,8 @@ def get_result(task_id: str) -> Dict[str, Any | None]:
         "status": result.status,
         "result": result.result if result.ready() else None
     }
-    
+
+
 @app.get("/status/{task_id}")
 def get_status(task_id: str) -> Dict[str, Any | None]:
     result = AsyncResult(task_id, app=celery_app)
