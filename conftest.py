e02c26dde420d47c3d1fa395e4149e3811dd81bc
Prateek Tandon <ptandon1@andrew.cmu.edu>
2025-04-23 08:50:00 -0700

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 @pytest.fixture(autouse=True, scope="session")
 def configure_celery_for_testing():
-    from main import app as celery_app
+    from celery_worker import celery_app
     celery_app.conf.update(
         task_always_eager=True,
         task_eager_propagates=True,
